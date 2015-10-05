19b63e5186f70b4eb550c0e74b1296f4368ec083
Taylor Monacelli <taylor.monacelli@streambox.com>
2015-10-04 18:17:59 -0700

    Stow doesn't crequire all that build stuff

--- install.sh
+++ install.sh
@@ -2,13 +2,10 @@
 
 if yum --version >/dev/null 2>&1
 then
-	yum install -y make automake gcc gcc-c++ kernel-devel curl
-	# stow relies on perl's Test::Output, so fetch with cpan, but amazon
-	# awi doesn't provide cpan
-	yum -y install perl-CPAN
+	yum install -y curl make
 else
 	apt-get -y -qq update
-	apt-get -y -qq install build-essential libncurses-dev curl make
+	apt-get -y -qq install curl make
 fi
 
 stowver=2.1.3
@@ -18,6 +15,9 @@ cd /usr/local/src
 curl -O http://ftp.gnu.org/gnu/stow/stow-$stowver.tar.gz
 tar xvf stow-$stowver.tar.gz
 cd stow-$stowver
-PERL_MM_USE_DEFAULT=1 perl -MCPAN -e 'CPAN::install "Test::Output";'
-./configure
+mkdir -p /usr/local/stow
+./configure --prefix=/usr/local/stow/stow-$stowver
 make install
+
+cd /usr/local/stow
+stow stow-$stowver
